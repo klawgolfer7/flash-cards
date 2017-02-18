0cfa897cfbd1afe4050e51104e3b6020e02fc98b
Kevin Lawler <kevinrlawler@gmail.com>
2017-02-17 22:22:31 -0500

    initial commit

---
+++ basicFlashcard.js
@@ -0,0 +1,12 @@
+var basicFlashcard = function (frontCard, backCard) {
+	this.front = frontCard;
+	this.back = backCard;
+	this.showFront = function() {
+		return(this.front);
+	};
+	this.showBack = function() {
+		return(this.back);
+	};
+}
+
+module.exports = basicFlashcard;
